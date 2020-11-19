04992d418f4d6c0e190412c71979160cf5b13a67
BarenMaulana <barenmaulana@gmail.com>
2020-11-19 14:31:31 +0700

    mengubah validasi artikel

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -60,7 +60,7 @@ class ArticleController extends Controller
         $validate = $this->validate($request, [
             'article_title' => 'required|min:3',
             'article_content' => 'required|min:10',
-            'article_sub_content' => 'required|max:40',
+            'article_sub_content' => 'required|max:150',
             'image' => 'required|image|mimes:jpg,png,jpeg,svg|max:700'
         ]);
 
@@ -119,7 +119,7 @@ class ArticleController extends Controller
             $validate = $this->validate($request, [
                 'article_title' => 'required|min:3',
                 'article_content' => 'required|min:10',
-                'article_sub_content' => 'required|max:40',
+                'article_sub_content' => 'required|max:150',
                 'image' => 'required|image|mimes:jpg,png,jpeg|max:700'
             ]);
 
